8f3da3a462e81ae54a929318ef9d5c91cfb3a675
theo begyn <theobegyn@live.com>
2022-12-29 15:12:38 +0100

    Add new components to ReactWidgetFactory

--- src/lib/Components/index.tsx
+++ src/lib/Components/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
-import {CheckBox, CheckRadioBoxProps} from "./Select";
+import {Button, ButtonProps} from "./Button";
+import { Tabs, TabsProps } from "./Menu";
+import {CheckBox, RadioBox, CheckRadioBoxProps} from "./Select";
 import {TextBox, TextBoxProps} from "./TextArea";
 
 export * from "./Button";
@@ -29,9 +31,47 @@ export default class ReactWidgetFactory {
 		root.render(<TextBox {..._props} />);
     }
 
+	/**
+	 * Create checkbox
+	 * 
+	 * @param props Checkbox properties
+	 */
 	public static createCheckBox(props: CheckRadioBoxProps) {
 		const _props: CheckRadioBoxProps = { ...props };
 		const root = createRoot(document.getElementById(_props.ContainerId) as HTMLElement);
 		root.render(<CheckBox {..._props} />);
+	}
+
+	/**
+	 * Create radiobox
+	 * 
+	 * @param props Radiobox properties
+	 */
+	public static createRadioBox(props: CheckRadioBoxProps) {
+		const _props: CheckRadioBoxProps = { ...props };
+		const root = createRoot(document.getElementById(_props.ContainerId) as HTMLElement);
+		root.render(<RadioBox {..._props} />);
+	}
+
+	/**
+	 * Create button
+	 * 
+	 * @param props Button properties
+	 */
+	public static createButton(props: ButtonProps) {
+		const _props: ButtonProps = { ...props };
+		const root = createRoot(document.getElementById(_props.ContainerId) as HTMLElement);
+		root.render(<Button {..._props} />);
+	}
+
+	/**
+	 * Create tabs
+	 * 
+	 * @param props Tabs properties
+	 */
+	public static createTabs(props: TabsProps) {
+		const _props: TabsProps = { ...props };
+		const root = createRoot(document.getElementById(_props.ContainerId) as HTMLElement);
+		root.render(<Tabs {..._props} />);
 	}
 }
